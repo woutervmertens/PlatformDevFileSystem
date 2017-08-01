942be3fd11d5b0ef75a9461c2615c7bb496723a3
woutervmertens <woutervmertens@gmail.com>
2017-08-01 12:45:14 +0200

    Changed apple path

--- PlatformDevFileSystem/main.cpp
+++ PlatformDevFileSystem/main.cpp
@@ -4,6 +4,8 @@
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 #define PATH "H:/repos/PlatformDevFileSystem/PlatformDevFileSystem/test"
+#elif defined(__APPLE__)
+#define PATH "~/Documents/PD"
 #elif defined(unix) || defined(__unix) || defined(__unix__)
 #define PATH "/home/wouter/PD"
 #else
